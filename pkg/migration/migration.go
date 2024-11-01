29b5031531c61d1ef37f1edcd62999be8bfb7b9c
Esa <ehsan_amd72@hotmail.com>
2024-11-01 09:09:25 +0200

    Load Models from migration Directory

---
+++ pkg/migration/migration.go
@@ -0,0 +1,37 @@
+package migration
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/esa-kian/shredder/pkg/models"
+)
+
+func LoadModelsFromMigrationDir() ([]models.Model, error) {
+	var modelsList []models.Model
+
+	files, err := os.ReadDir(".")
+	if err != nil {
+		return nil, fmt.Errorf("failed to read migration directory: %w", err)
+	}
+
+	for _, file := range files {
+		if filepath.Ext(file.Name()) == ".json" {
+			data, err := os.ReadFile(filepath.Join("./migration", file.Name()))
+			if err != nil {
+				return nil, fmt.Errorf("failed to read model file %s: %w", file.Name(), err)
+			}
+
+			var model models.Model
+			if err := json.Unmarshal(data, &model); err != nil {
+				return nil, fmt.Errorf("failed to parse model file %s: %w", file.Name(), err)
+			}
+
+			modelsList = append(modelsList, model)
+		}
+	}
+
+	return modelsList, nil
+}
