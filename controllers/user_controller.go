0e72180ba89f4ae15ee0f395175cdb2b5bdbc09a
Esa <ehsan_amd72@hotmail.com>
2024-11-01 09:10:05 +0200

    Integrate Migration into the Main Program

---
+++ controllers/user_controller.go
@@ -0,0 +1,10 @@
+
+		package controllers
+
+		import ( "net/http" )
+
+		// userController handles CRUD operations for the user model type userController struct { // Add necessary fields here }
+
+		// NewuserController initializes the controller for user func NewuserController() *userController { return &userController{} }
+
+		// Example of a Create handler func (c *userController) Create(w http.ResponseWriter, r *http.Request) { // TODO: Implement Create } 
